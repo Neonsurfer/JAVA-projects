0d611dcf8aa63897c56bc1a430f6245eebe0ee67
Neonsurfer <vmicuka9@gmail.com>
2019-04-02 15:13:47 +0200

    P3_Arrays
    Arrays

---
+++ P3_Arrays/src/p3_arrays/P3_Arrays.java
@@ -0,0 +1,33 @@
+package p3_arrays;
+
+public class P3_Arrays {
+
+    public static void main(String[] args) {
+        
+        /*
+        
+        Let's say we want to store 10 integers.
+        We could define 10 variables, but that would not be efficient, or easy.
+        For this reason, we use arrays.
+        
+        Lets initialize 2 arrays for ints to show the 2 ways to do it
+        */
+        
+        int [] array1 = {0,1,2,3,4,5};
+        int [] array2 = new int [10];
+        
+        /*
+        
+        The first array is initialized, meaning it is declared, and values are assigned to each of its "slots".
+        The second array is only defined, because all of it's "slots" contain null values.
+        
+        And here we run into the biggest problem with basic arrays.
+        Their size is fixed.
+        a1rray's length is 6, array2's length is 10 (indexed 0-9).
+        
+        
+        */
+        
+    }
+    
+}
